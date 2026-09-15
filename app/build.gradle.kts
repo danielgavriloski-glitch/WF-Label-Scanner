d928b1c4c59e715f03b8fa5a170c3622e2e0185b
danielgavriloski-glitch <danielgavriloski@gmail.com>
2026-09-15 10:04:31 +0200

    Release WF Label Scanner v5.7 aligned cells

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "mk.wf.labelscanner"
         minSdk = 24
         targetSdk = 35
-        versionCode = 28
-        versionName = "5.6-multi-size"
+        versionCode = 29
+        versionName = "5.7-aligned-cells"
     }
 
     compileOptions {
