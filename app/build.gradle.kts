e711cecbd3ffb79cb2c0d326ea647a18274abfc8
danielgavriloski-glitch <danielgavriloski@gmail.com>
2026-09-15 09:48:52 +0200

    Release WF Label Scanner v5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "mk.wf.labelscanner"
         minSdk = 24
         targetSdk = 35
-        versionCode = 26
-        versionName = "5.4-spatial-anchors"
+        versionCode = 27
+        versionName = "5.5-confirmed-save"
     }
 
     compileOptions {
